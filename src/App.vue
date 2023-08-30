cd40cbde4ee71ac898101933e1708a6d406be22d
arigkubra <arigkubra@gmail.com>
2023-08-30 22:42:22 +0300

    App.vue edited

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,35 @@
 <template>
   <div class="bg-zinc-800 h-screen">
 
-    <div class="flex flex-col justify-center items-center ">
+    <AddSection />
 
-      <h1 class="font-bold text-3xl pt-10 pb-7 text-white">Todo App</h1>
-
-      <!-- Input Area -->
-
-      <div class="form-control mb-5 w-full max-w-xs">
-        <label class="label">
-          <span class="label-text text-white">New ToDo</span>
-        </label>
-        <input
-          type="text"
-          placeholder="Enter Task..."
-          class="input input-bordered w-full max-w-xs"
-        />
-      </div>
-    </div>
 
     <!-- Buttons -->
-    
+
     <div class="flex justify-center items-center">
-      
       <buttons class="btn-neutral">Clear Todos</buttons>
       <buttons class="ml-10 btn-primary">Add Todos</buttons>
-      
     </div>
-    
+
     <TodoItem />
   </div>
 </template>
 
 <script>
+import AddSection from "./components/AddSection.vue";
 import TodoItem from "./components/TodoItem.vue";
 import Buttons from "./components/Buttons.vue";
 export default {
   name: "App",
   components: {
+    AddSection,
     TodoItem,
     Buttons,
+  },
+  methods: {
+    addNewTodo(e) {
+      console.log(event.target.value);
+    },
   },
 };
 </script>
