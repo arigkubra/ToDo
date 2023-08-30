90585b350f395c81cea4798eabaebd71802a12f4
arigkubra <arigkubra@gmail.com>
2023-08-30 21:26:57 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-zinc-800 h-screen">
+
+    <div class="flex flex-col justify-center items-center ">
+
+      <h1 class="font-bold text-3xl pt-10 pb-7 text-white">Todo App</h1>
+
+      <!-- Input Area -->
+
+      <div class="form-control mb-5 w-full max-w-xs">
+        <label class="label">
+          <span class="label-text text-white">New ToDo</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Task..."
+          class="input input-bordered w-full max-w-xs"
+        />
+      </div>
+    </div>
+
+    <!-- Buttons -->
+    
+    <div class="flex justify-center items-center">
+      
+      <buttons class="btn-neutral">Clear Todos</buttons>
+      <buttons class="ml-10 btn-primary">Add Todos</buttons>
+      
+    </div>
+    
+    <TodoItem />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoItem from "./components/TodoItem.vue";
+import Buttons from "./components/Buttons.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoItem,
+    Buttons,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
