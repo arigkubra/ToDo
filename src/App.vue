e6f66649508f23c68f39abf79d68d6a676ed39cc
Emirhan Yagci <emirhan.yacis@gmail.com>
2023-09-04 16:10:55 +0300

    clear all function bug fixed

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       Todo App
     </h1>
 
-    <AddSection :addItem="addItem" />
+    <AddSection :addItem="addItem" :clearAllHandler="clearAll" />
 
     <ListSection />
   </div>
@@ -16,21 +16,21 @@
 import AddSection from "./components/AddSection.vue";
 import ListSection from "./components/ListSection.vue";
 export default {
-  data(){
-    return{
+  data() {
+    return {
       getData: {
         todoList: [
-          { id: 1, text:"First Todo" },
-          { id: 2, text:"Second Todo" },
-        ]
-      }
-    }
+          { id: 1, text: "First Todo" },
+          { id: 2, text: "Second Todo" },
+        ],
+      },
+    };
   },
-  provide(){
-    return{
+  provide() {
+    return {
       getData: this.getData,
-      deleteItem : this.deleteItem
-    }
+      deleteItem: this.deleteItem,
+    };
   },
   components: {
     AddSection,
@@ -41,19 +41,17 @@ export default {
       this.getData.todoList.push({
         id: new Date().getTime(), //unique bir değer oluşturmak için şu anki saati ve tarihi milisaniye cinsinden bir sayıya dönüştürür. Id oluşturmak için kullanılır.
         text: todo,
-        });
-      },
-    clearAll(todoList){
-      
+      });
+    },
+    clearAll(todoList) {
+      this.getData.todoList = [];
+      console.log("test");
     },
     deleteItem(todoItem) {
-      this.getData.todoList = this.getData.todoList.filter((t) => t != todoItem)
-      }
+      this.getData.todoList = this.getData.todoList.filter(
+        (t) => t != todoItem
+      );
     },
-    
- 
-  
+  },
 };
 </script>
-
-
